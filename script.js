542ca4ad1ece75c82dd7155bba6f4102d2eccf3b
Bertmoss <b3rtm0ss@gmail.com>
2022-05-18 15:39:30 +0200

    Add player factory function and displayController module

--- script.js
+++ script.js
@@ -1,62 +1,94 @@
 "use strict";
 
+/* GAMEBOARD */
 const gameBoardModule = (() => {
-  const gameBoard = {
-    gameBoardArr: [] /* renders the divs */,
-    emptyDivsArr: [] /*shows which divs are still free/empty*/,
-  };
-
+  const gameBoardArr = [];
   const _container = document.querySelector(".container");
 
+  /* creates a grid cell */
   function _createGameDiv() {
     const gameDiv = document.createElement("div");
     gameDiv.classList.add("game-div");
-    gameDiv.setAttribute("data-position", gameBoard.gameBoardArr.length);
-    gameDiv.addEventListener("click", () => {
-      if (!gameDiv.textContent) {
-        let thisDiv =
-          gameDiv.getAttribute(
-            "data-position"
-          ); /* locates the div that was clicked */
-        let position = gameBoard.emptyDivsArr.indexOf(
-          +thisDiv
-        ); /* finds the number of the div that was clicked in Empty divs */
-        gameDiv.textContent = "X";
-        gameBoard.emptyDivsArr.splice(
-          position,
-          1
-        ); /* removes the div that was clicked from Empty divs  */
-      }
-    });
-    gameBoard.emptyDivsArr.push(gameBoard.gameBoardArr.length);
-    gameBoard.gameBoardArr.push(gameDiv);
+    gameDiv.setAttribute("data-position", gameBoardArr.length);
+    gameBoardArr.push(gameDiv);
   }
-
+  /* creates all the grid cells */
   function _createAllGameDivs() {
     for (let i = 1; i <= 9; i++) {
       _createGameDiv();
     }
   }
-
+  /* appends the grid cells to the DOM */
   function displayGameDivs() {
     _createAllGameDivs();
-    for (let div of gameBoard.gameBoardArr) {
+    for (let div of gameBoardArr) {
       _container.appendChild(div);
     }
   }
 
   return {
     displayGameDivs: displayGameDivs,
-    gameBoard: gameBoard,
+    gameBoardArr: gameBoardArr,
   };
 })();
 
 gameBoardModule.displayGameDivs();
 
+const displayController = (() => {
+  let turn = "player 1";
+  const _gameDivs = document.querySelectorAll(".game-div");
+   
+  _gameDivs.forEach((gameDiv) => {
+    gameDiv.addEventListener("click", () => {
+      if (!gameDiv.textContent && turn === "player 1") {
+        gameDiv.textContent = "X";
+        turn = "player 2";
+      } else if (!gameDiv.textContent && turn === "player 2") {
+        gameDiv.textContent = "O";
+        turn = "player 1";
+      }
+    });
+  });
+  return {turn};
+})();
+
+
+const Player = (playerName) => {
+  const name = playerName;
+
+  return {
+    name,
+  };
+};
+
+let John = Player("John");
+console.log(John);
+
+let John2 = Player("John2");
+console.log(John2);
+
 /* The general idea is that the player will click on any of the divs and then the opponent will choose a random num from the empty divs arr 
 and will add text content to the div with the corresponding data-position  */
 /* 
-const displayModule = (()=> {
-  const 
 
-})(); */
+
+
+//GameDiv listener
+/* gameDiv.addEventListener("click", () => {
+  if (!gameDiv.textContent) {
+    let thisDiv =
+      gameDiv.getAttribute(
+        "data-position"
+      ); //locates the div that was clicked 
+    let position = gameBoard.emptyDivsArr.indexOf(
+      +thisDiv
+    ); // finds the number of the div that was clicked in Empty divs 
+    gameDiv.textContent = "X";
+    gameBoard.emptyDivsArr.splice(
+      position,
+      1
+    ); //removes the div that was clicked from Empty divs  
+  }
+});
+gameBoard.emptyDivsArr.push(gameBoard.gameBoardArr.length);
+ */
